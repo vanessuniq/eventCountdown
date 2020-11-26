4196ae6385042e949854ff7272caa1d595c02674
vanessuniq <vanessuniq@gmail.com>
2020-11-25 23:42:37 -0500

    adjusted style

--- src/App.js
+++ src/App.js
@@ -41,8 +41,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div>
-        <form onSubmit={formSubmit}>
+      <div style={{borderColor: '#fff', borderStyle: 'groove'}}>
+        <form style={{marginBottom: '30px'}} onSubmit={formSubmit}>
           <div style={{display: 'flex'}}>
             <div className="form-field">
               <label htmlFor="name"><h3>Event Name</h3></label>
@@ -52,7 +52,7 @@ function App() {
               <label htmlFor="date"><h3>Event Date</h3></label>
               <input type="date" name="date" value={event.date} onChange={inputChange}/>
             </div>
-            <div className="form-field" style={{marginTop: '100px'}}>
+            <div className="form-field" style={{marginTop: '60px'}}>
               <button type="submit" disabled={!(event.name && event.date)}>Start Countdown</button>
             </div>
           </div>
